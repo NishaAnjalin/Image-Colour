7a9cefa788c204fcb517a05ce7dd416dd71aa284
NishaAnjalin <139215902+NishaAnjalin@users.noreply.github.com>
2025-08-04 15:22:46 +0530

    Add files via upload
    In the real world, this code could be used in a photo management or surveillance system to automatically display and store captured images for backup or future analysis.

---
+++ im.py
@@ -0,0 +1,9 @@
+import cv2
+img = cv2.imread("book.png") #Read an image
+
+cv2.imshow('Show', img) #To display the image
+
+cv2.imwrite('Harry-potter.jpg', img) #To save the image
+
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
